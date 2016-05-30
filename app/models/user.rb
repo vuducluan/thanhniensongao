54efc961b1289fd7c1584ff5994892990ae6d9f6
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-05-30 13:46:56 +0700

    init model+ relationship + gem devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_dates
+  has_many :eating_days, through: :user_date
+end
---
+++ db/migrate/20160530063924_create_eating_days.rb
@@ -0,0 +1,10 @@
+class CreateEatingDays < ActiveRecord::Migration
+  def change
+    create_table :eating_days do |t|
+      t.string :date
+      t.float :money
+
+      t.timestamps null: false
+    end
+  end
+end
