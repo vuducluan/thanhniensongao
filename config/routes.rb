229f8d64ae6a052706ad7e914c746b3fd79a0030
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-06-03 09:58:09 +0700

    Fix Edit User

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'eating_days/new'
-
-  get 'eating_days/show'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users, only: [:registration, :session]
 
   root "users#index"
