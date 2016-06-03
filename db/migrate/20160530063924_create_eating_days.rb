e727584c907d06f8d7f5138f9522eaeb4ad304cb
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-06-03 17:31:48 +0700

    bundle install

--- db/migrate/20160530063924_create_eating_days.rb
+++ db/migrate/20160530063924_create_eating_days.rb
@@ -2,7 +2,7 @@ class CreateEatingDays < ActiveRecord::Migration
   def change
     create_table :eating_days do |t|
       t.string :date
-      t.float :money
+      t.float :money, default: 0
       t.integer :meal
 
       t.timestamps null: false
